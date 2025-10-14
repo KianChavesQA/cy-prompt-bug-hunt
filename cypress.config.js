3250d43d5d083807ce8243366b99247f3cccb0c0
Walmyr <wlsf82@gmail.com>
2025-10-14 22:13:42 +0200

    Remove not needed flag from the config
    Now that Cypress Studio is in GA (General Availability), there's no
    need for the experimental flag anymore.

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "496opc",
   e2e: {
-    experimentalStudio: true,
     experimentalPromptCommand: true,
     fixturesFolder: false,
     supportFile: false,
